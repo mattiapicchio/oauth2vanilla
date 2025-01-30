f945ab43b88f6009328f01306dd367c153c7c103
Mattia Picchio <picchiomattia@gmail.com>
2025-01-30 18:34:36 +0100

    chore: add tailwind config

---
+++ src/assets/css/tailwindColors.ts
@@ -0,0 +1,54 @@
+export const COLORS = {
+  gray: {
+    DEFAULT: '#0F1113',
+    dark: '#181C1F',
+    soft: '#CCCCCC',
+  },
+  black: {
+    DEFAULT: '#000',
+    rich: '#040307',
+  },
+  current: 'currentColor',
+  transparent: 'transparent',
+  sunflare: '#F7C927',
+  voltgreen: '#6CF401',
+  'brand-orange': '#FF4500',
+  'brand-blue': '#0079D3',
+  background: 'hsl(var(--background))',
+  foreground: 'hsl(var(--foreground))',
+  border: 'hsl(var(--border))',
+  input: 'hsl(var(--input))',
+  ring: 'hsl(var(--ring))',
+  primary: {
+    DEFAULT: 'hsl(var(--primary))',
+    foreground: 'hsl(var(--primary-foreground))',
+  },
+  secondary: {
+    DEFAULT: 'hsl(var(--secondary))',
+    foreground: 'hsl(var(--secondary-foreground))',
+  },
+  destructive: {
+    DEFAULT: 'hsl(var(--destructive))',
+    foreground: 'hsl(var(--destructive-foreground))',
+  },
+  warning: {
+    DEFAULT: 'hsl(var(--warning))',
+    foreground: 'hsl(var(--warning-foreground))',
+  },
+  muted: {
+    DEFAULT: 'hsl(var(--muted))',
+    foreground: 'hsl(var(--muted-foreground))',
+  },
+  accent: {
+    DEFAULT: 'hsl(var(--accent))',
+    foreground: 'hsl(var(--accent-foreground))',
+  },
+  popover: {
+    DEFAULT: 'hsl(var(--popover))',
+    foreground: 'hsl(var(--popover-foreground))',
+  },
+  card: {
+    DEFAULT: 'hsl(var(--card))',
+    foreground: 'hsl(var(--card-foreground))',
+  },
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,81 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
+import { COLORS } from './src/assets/css/tailwindColors'
 
-export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+const config: Config = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  darkMode: 'class', // disable automatic dark mode
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      colors: COLORS,
+    },
+    container: {
+      center: true,
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1440px',
+        '3xl': '1536px',
       },
+    },
+    boxShadow: {
+      'custom-light': '0 4px 8px rgba(255, 255, 255, 0.2)',
+    },
+    borderRadius: {
+      lg: `var(--radius)`,
+      md: `calc(var(--radius) - 2px)`,
+      sm: 'calc(var(--radius) - 4px)',
+    },
+    fontSize: {
+      large: ['1.75rem', '1.313rem'], // to replace
+      xlHeading: [
+        '2rem',
+        {
+          lineHeight: '2rem',
+          letterSpacing: '-1.6px',
+          fontWeight: '900',
+        },
+      ],
+      largeHeading: [
+        '1.75rem',
+        {
+          lineHeight: '1.75rem',
+          letterSpacing: '-1.4px',
+          fontWeight: '700',
+        },
+      ],
+      mediumHeading: [
+        '1.5rem',
+        {
+          lineHeight: '1.35rem',
+          fontWeight: '600',
+        },
+      ],
+      smallHeading: [
+        '1.125rem',
+        {
+          lineHeight: '0.875rem',
+          letterSpacing: '-0.7px',
+          fontWeight: '600',
+        },
+      ],
+      body: [
+        '1rem',
+        {
+          lineHeight: '1.5rem',
+          fontWeight: '400',
+        },
+      ],
+      label: [
+        '1rem',
+        {
+          lineHeight: '1rem',
+          fontWeight: '500',
+        },
+      ],
     },
   },
   plugins: [],
-} satisfies Config;
+}
+export default config
