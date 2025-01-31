56a19d9859638ee4607d860a83f6003c9aa16efc
Mattia Picchio <picchiomattia@gmail.com>
2025-01-31 23:45:19 +0100

    feat: integrate LandingPage component into Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import LandingPage from '@/features/landingPage/LandingPage'
+
 export default async function Home() {
   return (
     <div className="flex-col justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       Oauth2 Sandbox
+      <LandingPage />
     </div>
   )
 }
