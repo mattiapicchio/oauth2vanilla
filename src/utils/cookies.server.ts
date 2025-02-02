4ac1bd840b97cc4de7dfb10e52d74cc26501260f
Mattia Picchio <picchiomattia@gmail.com>
2025-02-02 18:21:50 +0100

    feat: add setCookie utility function

--- src/utils/cookies.server.ts
+++ src/utils/cookies.server.ts
@@ -9,9 +9,14 @@ export async function getCookie(key: string) {
   const cookieStore = await cookies()
   const cookie = cookieStore.get(key)
 
-  if (cookie && cookie.value) {
+  if (cookie) {
     return cookie.value
   }
+}
+
+export async function setCookie(key: string, value: string) {
+  const cookieStore = await cookies()
+  cookieStore.set(key, value, { secure: true })
 }
 
 export async function checkCookie(key: string) {
